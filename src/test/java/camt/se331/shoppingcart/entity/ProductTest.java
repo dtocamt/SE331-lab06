47049b3df2c4471964f4869633f9f0a0fcab8e56
dto gmail <c.doungsaard@gmail.com>
2015-02-25 08:00:08 +0700

    finish question 1.3 add theProductTest class

---
+++ src/test/java/camt/se331/shoppingcart/entity/ProductTest.java
@@ -0,0 +1,32 @@
+package camt.se331.shoppingcart.entity;
+
+import camt.se331.shoppingcart.dao.NewSimpleProductDao;
+import camt.se331.shoppingcart.dao.ProductDao;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+/**
+ * Created by CAMT on 2/25/2015.
+ */
+public class ProductTest{
+
+    ProductDao productDao;
+
+    @Before
+    public void setup(){
+        productDao = new NewSimpleProductDao();
+    }
+
+    @Test
+    public void testProductGetNetPrice(){
+        VatEntity.getInstance().setVat(0.1);
+        assertThat(productDao.getProduct(1l).getNetPrice(), is(900.00));
+        VatEntity.getInstance().setVat(0.05);
+        assertThat(productDao.getProduct(2l).getNetPrice(),is(1900.00));
+
+
+    }
+
+}
